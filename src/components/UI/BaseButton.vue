42a0942a5391c5afc332ca4f658c312ad0ad1630
mehm8128 <mehm8128@gmail.com>
2023-01-10 22:42:50 +0900

    add disabled

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -3,13 +3,14 @@ type ButtonType = 'primary'
 const props = withDefaults(
   defineProps<{
     type: ButtonType
+    disabled?: boolean
   }>(),
   { type: 'primary' }
 )
 </script>
 
 <template>
-  <button :class="$style.button" :data-type="props.type">
+  <button :class="$style.button" :data-type="props.type" :disabled="disabled">
     <slot />
   </button>
 </template>
@@ -20,5 +21,14 @@ const props = withDefaults(
   background-color: #ff007f;
   border-radius: 4px;
   padding: 0.5rem;
+
+  &:hover {
+    opacity: 0.7;
+    transition: all 0.2s ease-in-out;
+  }
+  &:disabled {
+    opacity: 0.3;
+    cursor: not-allowed;
+  }
 }
 </style>
