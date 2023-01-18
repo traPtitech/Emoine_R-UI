0d7dd21ad81aedf30526247a591172e2b6d30d48
mehm8128 <mehm8128@gmail.com>
2023-01-19 00:04:57 +0900

    add navigation bar

---
+++ src/components/UI/PaginationBar.vue
@@ -0,0 +1,92 @@
+<script lang="ts" setup>
+import AIcon from '@/components/UI/AIcon.vue'
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+interface Props {
+  currentPage: number
+  totalPages: number
+}
+
+const props = defineProps<Props>()
+
+const pageButtons = computed(() => {
+  if (props.currentPage <= 3) {
+    return [...Array(5).keys()].map(i => i + 1)
+  }
+  if (props.currentPage >= props.totalPages - 2) {
+    return [...Array(5).keys()].map(i => i + props.totalPages - 4)
+  }
+  return [...Array(5).keys()].map(i => i - 2 + props.currentPage)
+})
+</script>
+
+<template>
+  <div :class="$style.container">
+    <router-link
+      :to="`?page=${currentPage - 1}`"
+      :class="$style.linkButton"
+      :disabled="currentPage === 1"
+    >
+      <a-icon name="mdi:chevron-left" :class="$style.chevronIcon" />
+    </router-link>
+
+    <div :class="$style.pageButtonContainer">
+      <router-link
+        v-for="page in pageButtons"
+        :key="page"
+        :class="$style.linkButton"
+        :disabled="page === currentPage"
+        :to="`?page=${page}`"
+      >
+        {{ page }}
+      </router-link>
+    </div>
+
+    <router-link
+      :to="`?page=${currentPage + 1}`"
+      :class="$style.linkButton"
+      :disabled="currentPage === totalPages"
+    >
+      <a-icon name="mdi:chevron-right" :class="$style.chevronIcon" />
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  background-color: #f6f6f6;
+  padding: 100px;
+}
+.pageButtonContainer {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+}
+.linkButton {
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #818181;
+  background-color: #fff;
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 24px;
+
+  &[disabled='true'] {
+    color: #fff;
+    background-color: #818181;
+    cursor: default;
+    pointer-events: none;
+  }
+  &:hover {
+    color: #fff;
+    background-color: #818181;
+  }
+}
+</style>
