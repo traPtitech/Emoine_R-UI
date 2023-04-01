556cf833271397f4952747c624dfb4b94eb0f95e
uz4ki <uz4ki.mail@gmail.com>
2023-04-01 13:14:13 +0900

    fix2

--- src/utils/DayFormat.vue
+++ src/utils/DayFormat.vue
@@ -2,11 +2,14 @@
 import dayjs from 'dayjs'
 
 export function dayFormat(date: Date): string {
-  const date1 = dayjs(date)
-  if (dayjs().diff(date1, 'year')) return `${dayjs().diff(date1, 'year')}年前`
-  if (dayjs().diff(date1, 'month'))
-    return `${dayjs().diff(date1, 'month')}ヶ月前`
-  if (dayjs().diff(date1, 'day')) return `${dayjs().diff(date1, 'day')}日前`
-  return `${dayjs().diff(date1, 'hour')}時間前`
+  const pastDate = dayjs(date)
+  const nowDate = dayjs()
+  if (nowDate.diff(pastDate, 'year'))
+    return `${nowDate.diff(pastDate, 'year')}年前`
+  if (nowDate.diff(pastDate, 'month'))
+    return `${nowDate.diff(pastDate, 'month')}ヶ月前`
+  if (nowDate.diff(pastDate, 'day'))
+    return `${nowDate.diff(pastDate, 'day')}日前`
+  return `${nowDate.diff(pastDate, 'hour')}時間前`
 }
 </script>
