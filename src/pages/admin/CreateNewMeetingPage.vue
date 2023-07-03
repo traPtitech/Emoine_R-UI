a4763d5bd400d9d3ae1530d4522dce1ea6519b12
uz4ki <uz4ki.mail@gmail.com>
2023-07-03 23:10:29 +0900

    fix

--- src/pages/admin/CreateNewMeetingPage.vue
+++ src/pages/admin/CreateNewMeetingPage.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import apis, { CreateMeeting, Meeting } from '@/lib/apis'
+import apis, { CreateMeetingRequest } from '@/lib/apis'
 import BaseInput from '@/components/UI/BaseInput.vue'
 import BaseButton from '@/components/UI/BaseButton.vue'
 import EmoineHeader from '@/components/EmoineHeader.vue'
@@ -10,27 +10,31 @@ import router from '@/router'
 const headerLogo = '/logo.png'
 
 const liveUrl = ref('')
-const isUrlValidated = ref(true)
+const isValidURL = ref(true)
 
-function getLiveIdFromUrl(): string {
-  const params = new URL(liveUrl.value).searchParams
-  if (params.has('v')) return
+const getLiveIdFromUrl = (liveUrl: string) => {
+  const params = new URL(liveUrl).searchParams
+  if (!params.get('v')) throw new Error('Invalid URL')
   return params.get('v')
 }
 
 const createMeeting = async () => {
   let id = ''
   try {
-    const meetingInfo: CreateMeeting = {
-      video_id: getLiveIdFromUrl(),
+    const videoId = getLiveIdFromUrl(liveUrl.value)
+    if (!videoId) throw new Error('Invalid URL')
+    const meetingInfo: CreateMeetingRequest = {
+      video_id: videoId,
       description: ''
     }
     const res = (await apis.createMeeting(meetingInfo)).data
     id = res.id
   } catch (e: unknown) {
-    console.log(e)
-    isUrlValidated.value = false
-    return
+    if (e instanceof Error) {
+      console.log(e.message)
+      isValidURL.value = false
+      return
+    }
   }
 
   // TODO: 遷移先の名前決定したらここに書く
@@ -47,13 +51,16 @@ const createMeeting = async () => {
     <div :class="$style.cotainer">
       <div :class="$style.subTitleContainer">
         <h3 :class="$style.subTitleText">LIVE URL</h3>
-        <div v-if="!isUrlValidated" :class="$style.errorText">
+        <div v-if="!isValidURL" :class="$style.errorText">
           ※ 存在しないURLです
         </div>
       </div>
       <div :class="$style.newEventInputCotainer">
         <div :class="$style.input">
-          <base-input placeholder="Input URL..." />
+          <base-input
+            :model-value="liveUrl"
+            placeholder="YoutubeのライブURLを入力..."
+          />
         </div>
         <base-button @click="createMeeting">追加</base-button>
       </div>
@@ -67,40 +74,40 @@ const createMeeting = async () => {
   width: 1144px;
   flex-direction: column;
   align-items: center;
-  gap: 10px;
+  gap: 0.625rem;
   margin: 0 auto;
 }
 .cotainer {
   display: flex;
-  padding: 16px 20px;
+  padding: 1rem 1.25rem;
   flex-direction: column;
   align-items: flex-start;
-  gap: 10px;
+  gap: 0.625rem;
   align-self: stretch;
   background-color: white;
 }
 .title {
   display: flex;
-  padding: 7px 0px;
+  padding: 0.4375rem 0rem;
   align-items: center;
-  gap: 14px;
+  gap: 0.875rem;
   align-self: stretch;
 }
 .subTitleContainer {
   display: flex;
   align-items: flex-start;
-  gap: 20px;
+  gap: 1.25rem;
   align-self: stretch;
 }
 .subTitleText {
   color: #141414;
-  font-size: 24px;
+  font-size: 1.25rem;
   font-family: Arial;
   font-weight: 700;
 }
 .errorText {
   color: #ff007f;
-  font-size: 24px;
+  font-size: 1.25rem;
   font-family: Arial;
   font-style: normal;
   font-weight: 400;
@@ -109,7 +116,7 @@ const createMeeting = async () => {
 .newEventInputCotainer {
   display: flex;
   align-items: center;
-  gap: 10px;
+  gap: 0.625rem;
   align-self: stretch;
 }
 </style>
